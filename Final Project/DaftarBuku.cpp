5a8e162bcc776afc8cc15aec0ddc766d6b5e112d
Azzila Karen <azzilakaren@outlook.com>
2020-11-11 23:24:48 +0700

    ubah tampilan&member

--- Final Project/DaftarBuku.cpp
+++ Final Project/DaftarBuku.cpp
@@ -12,13 +12,17 @@ int databuku()
 	cout << " _____________________________________________________ " << endl;
 	cout << "|                                                     |" << endl;
 	cout << "|           Menu Buku Perpustakaan Amikom             |" << endl;
-	cout << "|           1.Buku Indonesia                          |" << endl;
-	cout << "|           2.Buku Inggris                            |" << endl;
-	cout << "|           3.Buku Pendidikan Pancasila               |" << endl;
-	cout << "|           4.Buku Matematika                         |" << endl;
+	cout << "|           1. Buku Indonesia                         |" << endl;
+	cout << "|           2. Buku Inggris                           |" << endl;
+	cout << "|           3. Buku Pendidikan Pancasila              |" << endl;
+	cout << "|           4. Buku Matematika						   |" << endl;
+	cout << "|           5. Buku Statistika                        |" << endl;
+	cout << "|           6. Buku Kewirausahaan                     |" << endl;
+	cout << "|           7. Buku Ekonomi                           |" << endl;
+	cout << "|           8. Buku Manajemen Usaha                   |" << endl;
 	cout << "|_____________________________________________________|" << endl;
 
-
+	cout << endl;
 
 	system("pause");
 	return false;
--- Final Project/MemberTeam.cpp
+++ Final Project/MemberTeam.cpp
@@ -4,10 +4,23 @@ using namespace std;
 
 int main()
 {
-	cout << "Nama Anggota:" << endl;
-	cout << "Nim Anggota:" << endl;
-	cout << "Kota Asal:" << endl;
+	cout << " \t BIODATA TEAM " << endl;
 
-
+	cout << " ------------------------------------------ " << endl;
+	cout << " |	1. | Rakandhiya Daani Rachmanto\n       | ";
+	cout << " | \t 20.11.3304\n                         | ";
+	cout << " + ....................................... + " << endl;
+	cout << " | 2. | Hasif Priyambudi\n                 | ";
+	cout << " | \t 20.11.3306\n                         | ";
+	cout << " + ....................................... + " << endl;
+	cout << " | 3. | Azzila Isme Karen\n                | ";
+	cout << " | \t 20.11.3312\n                         | ";
+	cout << " + ....................................... + " << endl;
+	cout << " | 4. | Sandy Wildani Bachtiar\n           | ";
+	cout << " | \t 20.11.3333\n                         | ";
+	cout << " + ....................................... + " << endl;
+	cout << " | 5. | Muhammad Abiyyu\n                  | ";
+	cout << " | \t 20.11.3346\n                         | ";
+	cout << " | --------------------------------------- | " << endl;
 
 }
